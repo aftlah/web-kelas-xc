4b311428d4c84247bdde89e02a31b06041082da8
Altaf Fattah <altaf.fattah05@gmail.com>
2023-11-27 13:15:01 +0700

    fix feat(kirim pesan)

--- src/app/components/Pages/Pesan.js
+++ src/app/components/Pages/Pesan.js
@@ -1,35 +1,44 @@
 'use client'
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import InputCard from '../elemets/InputCard'
+import Swal from 'sweetalert2'
 
 const Pesan = () => {
-  const [nama,setNama] = useState('')
-  const [nim,setNim] = useState('')
+  const [nama, setNama] = useState('')
+  const [nim, setNim] = useState('')
   const [textarea, setTextarea] = useState('')
-
-  useEffect(() => {
-    console.log(nama,nim,textarea);
-  },[nama,nim,textarea])
 
   const onPesanSubmit = (e) => {
     e.preventDefault()
-    console.log(nama,nim);
+    const data = {
+      name: nama,
+      nima: nim,
+      pesan: textarea
+    }
+    Swal.fire({
+      title: 'Pesan Berhasil Terkirim',
+      text: 'Terimakasih',
+      icon: 'success',
+      confirmButtonText: 'Oke'
+    })
+    console.log(data);
+    setNim('')
+    setNama('')
+    setTextarea('')
   }
   return (
     <>
       <h1 className='mt-10 px-1 font-black text-secondary text-xl'>KIRIM PESAN</h1>
       <form className='p-[.18rem]' onSubmit={onPesanSubmit}>
-        <InputCard Type="Nama" onChange={(e) => setNama(e.target.value)} />
-        <InputCard Type="Nim" onChange={(e) => setNim(e.target.value)}/>
+        <InputCard Type="Nama" onChange={(e) => setNama(e.target.value)}  value={nama}/>
+        <InputCard Type="Nim" onChange={(e) => setNim(e.target.value)}  value={nim}/>
         <div className='bg-white my-4 flex flex-col  rounded-lg '  >
           <label htmlFor="pesan" className='font-bold text-secondary'>Pesan</label>
-          <textarea onChange={(e) => setTextarea(e.target.value)} type="text" id="pesan" rows="4" className='border-secondary border-2 rounded-lg p-2' placeholder="Masukan Pesan" />
+          <textarea onChange={(e) => setTextarea(e.target.value)} value={textarea} type="text" id="pesan" rows="4" className='border-secondary border-2 rounded-lg p-2' placeholder="Masukan Pesan" />
         </div>
-        <div className=" w-full rounded-xl bg-primary text-center py-2.5 px-4 font-bold text-xl text-white ">
-          <button type="submit">
-            Kirim
-          </button>
-        </div>
+        <button type="submit" className='w-full rounded-xl bg-primary text-center py-2.5 px-4 font-bold text-xl text-white'>
+          Kirim
+        </button>
       </form>
     </>
   )
--- src/app/components/elemets/InputCard.js
+++ src/app/components/elemets/InputCard.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const InputCard = ({Type,onChange}) => {
+const InputCard = ({Type,onChange,value}) => {
   return (
     <>
      <div className='bg-white my-4 flex flex-col  rounded-lg '  >
           <label htmlFor={Type} className='font-bold text-secondary'>{Type}</label>
-          <input onChange={onChange} type="text" id={Type} className='p-2 border-secondary border-2 rounded-lg' placeholder={`Masukan ${Type}`}/>
+          <input onChange={onChange} type="text" id={Type} value={value} className='p-2 border-secondary border-2 rounded-lg' placeholder={`Masukan ${Type}`}/>
         </div>
     </>
   )
