629e9f91a029637b419b25d41e12da28fe521cbf
Altaf Fattah <altaf.fattah05@gmail.com>
2023-11-28 23:07:41 +0700

    fix responsive

--- src/app/components/Pages/JadwalMatkul.js
+++ src/app/components/Pages/JadwalMatkul.js
@@ -8,7 +8,7 @@ const JadwalMatkul = () => {
       <div className="bg-primary  rounded-xl mt-2 p-4 shadow-xl sm:text-start">
       <div className="text-white text-2xl font-bold">Senin <span className="text-sm ml-2">22-21-2022</span></div>
             <JadwalCard matkul="Analisis design dan algoritma" waktu="10.40 - 12.30" ruangan="4.3.3"/>
-            <JadwalCard matkul="Pemrograman Dasar" waktu="10.40 - 12.30" ruangan="Lab Kom"/>
+            <JadwalCard matkul="Bahasa Pemrograman Dasar" waktu="10.40 - 12.30" ruangan="Lab Kom"/>
       </div>
     </>
   )
--- src/app/components/elemets/JadwalCard.js
+++ src/app/components/elemets/JadwalCard.js
@@ -4,8 +4,8 @@ const JadwalCard = ({matkul,waktu,ruangan}) => {
     return (
         <>
             <div className="flex justify-between mt-5 text-white font-medium">
-                <div className=' w-[8rem]   sm:m-0'>{matkul}</div>
-                <div className='w-[10rem] sm:w-[12rem] flex text-center items-center flex-wrap border-x-2 '><span className='inline-block text-center  pl-1 pr-1 mx-auto'>{waktu}</span></div>
+                <div className=' w-[12rem]   sm:m-0'>{matkul}</div>
+                <div className='w-[10rem]  items-center text-center border-x-2 flex flex-wrap '><span className='inline-block text-center  pl-1 pr-1 mx-auto'>{waktu}</span></div>
                 <div className="font-semibold w-[5rem] text-center flex flex-wrap"><span className='inline-block m-auto '>{ruangan}</span></div>
             </div>
         </>
