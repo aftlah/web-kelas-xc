6b24501464d698c5fb436949ab4716927dba17dd
Altaf Fattah <altaf.fattah05@gmail.com>
2023-11-29 09:10:07 +0700

    fix scroll seminar

--- src/app/components/Pages/Navbar.js
+++ src/app/components/Pages/Navbar.js
@@ -6,12 +6,9 @@ import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 
 
-
-
 const Navbar = () => {
   const pathname = usePathname()
   const router = useRouter()
-
   const [data, setData] = useState('')
 
 
--- src/app/components/Pages/Seminar.js
+++ src/app/components/Pages/Seminar.js
@@ -4,8 +4,11 @@ import CardSeminar from '../elemets/CardSeminar'
 const Seminar = () => {
     return (
         <>
-            <div className='w-full  flex flex-col items-center sm:items-start sm:flex-row sm:overflow-x-scroll sm:gap-x-4 sm:w-[70rem]'>
+            <div className='w-full  flex flex-col items-center sm:items-start sm:flex-row  sm:gap-x-4 sm:w-max sm:mr-40'>
                 <CardSeminar image='/images/5.png' judul='Seminar 1' >
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                </CardSeminar>
+                <CardSeminar image='/images/5.png' judul='Seminar 1'  >
                     Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
                 </CardSeminar>
                 <CardSeminar image='/images/5.png' judul='Seminar 1'  >
