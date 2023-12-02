6626f61631aba36200267a1985af229205f8a927
Altaf Fattah <altaf.fattah05@gmail.com>
2023-12-02 09:07:57 +0700

    feat(SwiperJS: -Seminar)

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -25,7 +25,7 @@ const Login = () => {
   }
   return (
     <>
-      <div className="flex min-h-full mt-[10%]  flex-1 flex-col justify-center px-6 py-12 md:mt-0 lg:px-8">
+      <div className="flex min-h-full mt-[8%]  flex-1 flex-col justify-center px-6 py-12 sm:mt-[20%]  lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <Image priority src="/images/logo.png" width={150} height={150} alt="tesdt" className='mx-auto w-40 h-auto'/>
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
